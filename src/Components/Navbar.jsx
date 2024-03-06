dda25c8cbd7a3019e4d4cd3ff91dd4f17c6a2c9a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-06 01:15:39 +0100

    :art: responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./nStyle.css"
+import { FiMenu, FiX } from "react-icons/fi";
 
 export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
+
     <div className='nav'>
       <div className='nav-logo'>EV-olution</div>
-      <ul className='nav-menu'>
-        <li>Home</li>
-        <li>Explore</li>
-        <li>About</li>
-        <li className='nav-contact'>Contact</li>
+      <ul className={showMenu ? 'nav-menu nav-menu-mobile' : 'nav-menu'}>
+          <li>Home</li>
+          <li>Explore</li>
+          <li>About</li>
+          <li className='nav-contact'>Contact</li>
       </ul>
+      <div className='nav-menu-toggle' onClick={() => setShowMenu(!showMenu)}>
+          {showMenu ? <FiX /> : <FiMenu />}
+      </div>
     </div>
   )
 }
